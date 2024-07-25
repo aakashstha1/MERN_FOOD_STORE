ae0584f8fff90c281a97dea4b9357bb015c29d16
aakashstha1 <aakash.cerestha90@gmail.com>
2024-07-25 16:10:38 +0545

    day 55

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,21 +1,29 @@
 import mongoose from "mongoose";
 
+// Function to establish a connection to the MongoDB database
 export const dbConnection = async () => {
   try {
+    // Connect to MongoDB using the provided URI and database name from environment variables
     await mongoose.connect(process.env.MONGO_URI, {
       dbName: "FooDelivery",
     });
     console.log("Connected to MongoDB");
 
+    // Access the 'foodItems' and 'foodCategory' collections from the database
     const foodItemsCollection = mongoose.connection.db.collection("foodItems");
-    const foodCategoryCollection = mongoose.connection.db.collection("foodCategory");
+    const foodCategoryCollection =
+      mongoose.connection.db.collection("foodCategory");
 
+    // Fetch all documents from the 'foodItems' collection and store them in a global variable
     const foodItemsData = await foodItemsCollection.find({}).toArray();
+    // Fetch all documents from the 'foodCategory' collection and store them in a global variable
     const foodCategoryData = await foodCategoryCollection.find({}).toArray();
 
+    // Set global variables for easy access to the data across the application
     global.foodItems = foodItemsData;
     global.foodCategory = foodCategoryData;
   } catch (error) {
+    // Log an error message if the connection to MongoDB fails
     console.log(`Error connecting to MongoDB: ${error}`);
   }
 };
