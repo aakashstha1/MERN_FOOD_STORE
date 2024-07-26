d9b534fd52d356de781c7909cf59b279da5702a7
aakashstha1 <aakash.cerestha90@gmail.com>
2024-07-26 12:18:50 +0545

    Day56

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,3 +1,4 @@
+// Custom ErrorHandler class to create error instances with a status code
 class ErrorHandler extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -5,15 +6,19 @@ class ErrorHandler extends Error {
   }
 }
 
+// Middleware to handle errors in the application
 export const errorMiddleware = async (error, req, res, next) => {
+  // Default error message and status code if not provided
   error.message = error.message || "Internal Server Error";
   error.statusCode = error.statusCode || 500;
 
+  // Handle specific error types
   if (error.name === "CastError") {
     const message = `Invalid: Resource not found: ${error.message}`;
     error = new ErrorHandler(message, 404);
   }
 
+  // Send error response to the client
   return res.status(error.statusCode).json({
     success: false,
     message: error.message,
